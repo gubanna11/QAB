28d4f69f343b10433097f45c98d8989ae51d7300
Hanna Huba <gubanna11.gmail.com>
2023-09-05 23:04:19 +0300

    #1 Created database context with required tables

---
+++ QAB.API/QAB.Domain/Data/EntityConfiguration/CaseConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QAB.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAB.Domain.Data.EntityConfiguration
+{
+    public class CaseConfiguration : IEntityTypeConfiguration<Case>
+    {
+        public void Configure(EntityTypeBuilder<Case> builder)
+        {
+            List<Case> cases = new List<Case>()
+            {
+                new Case() {
+                    Id = 1,
+                    AuthorId = "author",
+                    Title = "Title Post",
+                    Content = "Content...",
+                    Description = "",
+                    IsActive = true,                    
+                    PublishDate = DateTime.Now,
+                    CaseTypeId = 1,
+                },
+                new Case() {
+                    Id = 2,
+                    AuthorId = "author",
+                    Title = "Title Question",
+                    Content = "Content...",
+                    Description = "",
+                    IsActive = true,
+                    PublishDate = DateTime.Now,
+                    CaseTypeId = 2,
+                },
+                new Case() {
+                    Id = 3,
+                    AuthorId = "author",
+                    Title = "Title Question",
+                    Content = "Content...",
+                    Description = "",
+                    IsActive = true,
+                    PublishDate = DateTime.Now,
+                    CaseTypeId = 2,
+                },
+                new Case() {
+                    Id = 4,
+                    AuthorId = "author",
+                    Title = "Title Question",
+                    Content = "Content...",
+                    Description = "",
+                    IsActive = false,
+                    PublishDate = DateTime.Now,
+                    CaseTypeId = 2,
+                },
+            };
+
+            builder.HasData(cases);
+        }
+    }
+}
---
+++ QAB.API/QAB.Domain/Data/EntityConfiguration/CaseTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QAB.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAB.Domain.Data.EntityConfiguration
+{
+    public class CaseTypeConfiguration : IEntityTypeConfiguration<CaseType>
+    {
+        public void Configure(EntityTypeBuilder<CaseType> builder)
+        {
+            List<CaseType> types = new List<CaseType>()
+            {
+                new CaseType()
+                {
+                    Id = 1,
+                    Name = "Post"
+                },
+                new CaseType()
+                {
+                    Id = 2,
+                    Name = "Question"
+                }
+            };
+
+            builder.HasData(types);
+        }
+    }
+}
