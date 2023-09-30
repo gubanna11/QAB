6dc381826c9e3ca0351d160594e7a8eda6d82be9
Hanna Huba <gubanna11.gmail.com>
2023-09-30 16:09:34 +0300

    #3 Created validator for create and update case dto; Changed max length for title and desc

---
+++ QAB.API/QAB.Services/Validators/CaseRequestDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using QAB.Services.Models.Dtos.Case;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAB.Services.Validators
+{
+    public class CaseRequestDtoValidator : AbstractValidator<CaseRequestDto>
+    {
+        public CaseRequestDtoValidator()
+        {
+            RuleFor(c => c.AuthorId)
+                .NotEmpty();
+
+            RuleFor(c => c.Title)
+                .NotEmpty()
+                .MaximumLength(500);
+
+            RuleFor(c => c.Description)
+                .NotEmpty()
+                .MaximumLength(1000);
+
+            RuleFor(c => c.Content)
+                .NotEmpty()
+                .MinimumLength(50)
+                .MaximumLength(120000);
+        }
+    }
+}
